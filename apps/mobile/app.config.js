2d3a244332c6b8e07a2264bc0805a0d3eb914a4f
alexcat84 <alexcat84@users.noreply.github.com>
2026-04-30 22:07:33 -0400

    chore(mobile): bump version to 3.1.3 (versionCode 6)

--- apps/mobile/app.config.js
+++ apps/mobile/app.config.js
@@ -7,7 +7,7 @@ module.exports = ({ config }) => ({
   ...config,
   name: "The Original I Ching",
   slug: "the-original-i-ching",
-  version: "3.1.2",
+  version: "3.1.3",
   scheme: "theoriginaliching",
   orientation: "portrait",
   platforms: ["android"],
@@ -24,7 +24,7 @@ module.exports = ({ config }) => ({
   },
   android: {
     package: "com.theoriginaliching.app",
-    versionCode: 5,
+    versionCode: 6,
     compileSdkVersion: 35,
     targetSdkVersion: 35,
     buildToolsVersion: "35.0.0",
